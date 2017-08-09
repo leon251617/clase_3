7e8a2afd35373b487f73dd7478c98f94f0468691
Francisco Javier León Ascencio <fjleona@darkstar.org>
2017-08-09 10:38:47 -0500

    programa que convierte furlongs a yardas

---
+++ conversor.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  float f;
+  float yd;
+  cout<<"Escriba su distancia en furlongs: ";
+  cin>>f;
+  cout<<"La distancia en yardas es : "<<220*f<<"\n";
+  
+	 
+  
+
+
+}
